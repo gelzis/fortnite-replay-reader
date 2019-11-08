d95e552a062b8096560ca03eb8ac359b0070249f
Gelzis <raivis.gelsbergs@gmail.com>
2019-11-08 19:24:09 +0200

    add total player count to the json output

--- cli.py
+++ cli.py
@@ -6,14 +6,21 @@ if len(sys.argv) < 2:
     raise Exception('Need to pass filename path!')
 
 with Reader(sys.argv[1]) as replay:
+
+    jsonOutput = {
+        'totalPlayers': replay.team_stats['total_players'],
+        'eliminations': [],
+    }
+
     eliminations = []
     for elim in replay.eliminations:
-        eliminations.append({
+        jsonOutput['eliminations'].append({
             'eliminated': elim.eliminated,
             'eliminator': elim.eliminator,
             'gun_type': elim.gun_type,
-            'time': elim.time.strftime('%Y-%m-%d %H:%I:%S'),
+            'time': elim.time.strftime('%H:%I:%S'),
             'knocked': elim.knocked,
             'weapon': elim.weapon,
         })
-    print(json.dumps(eliminations))
+
+    print(json.dumps(jsonOutput))
