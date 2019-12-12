07090cf916b17bd408de2b67a72fbc0a82de939a
Gelzis <raivis.gelsbergs@gmail.com>
2019-12-12 20:47:18 +0200

    update cli to match the new version, remove total players

--- cli.py
+++ cli.py
@@ -6,17 +6,15 @@ if len(sys.argv) < 2:
     raise Exception('Need to pass filename path!')
 
 with Reader(sys.argv[1]) as replay:
-
     jsonOutput = {
-        'totalPlayers': replay.team_stats['total_players'],
         'eliminations': [],
     }
 
     eliminations = []
     for elim in replay.eliminations:
         jsonOutput['eliminations'].append({
-            'eliminated': elim.eliminated,
-            'eliminator': elim.eliminator,
+            'eliminated': elim.eliminated.guid,
+            'eliminator': elim.eliminator.guid,
             'gun_type': elim.gun_type,
             'time': elim.time.strftime('%H:%I:%S'),
             'knocked': elim.knocked,
